cfd9fa45865c0f39112a691c1afd56783be5d55d
HeldyisAE <heaguero@estudiantec.cr>
2025-10-17 00:32:42 -0600

    Corrige nombre de la carpeta components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './Components/Header.jsx'
-import Container from './Components/Container.jsx'
+import Header from './components/Header.jsx'
+import Container from './components/Container.jsx'
 
 function App() {
   return (
